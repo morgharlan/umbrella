90c81eb409b449ceb018bb6d7a57c17dec26f284
morgharlan <morgharlan@gmail.com>
2025-04-10 18:39:08 +0000

    lat long

--- umbrella.rb
+++ umbrella.rb
@@ -3,20 +3,38 @@ require "http"
 require "json"
 require "dotenv/load"
 
+pp "Where are you located?"
+
+user_location = gets.chomp
+
+pp user_location
+
 # /.env
 
 gmaps_key = ENV.fetch("GMAPS_KEY")
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_key + "/41.8887,-87.6355"
+pirate_weather_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
-raw_response = HTTP.get(pirate_weather_url)
+response = HTTP.get(pirate_weather_url)
 
+raw_response = response.to_s
 
 parsed_response = JSON.parse(raw_response)
 
-currently_hash = parsed_response.fetch("currently")
+results = parsed_response.fetch("results")
 
-current_temp = currently_hash.fetch("temperature")
+first_result = results.at(0)
 
-puts "The current temperature is " + current_temp.to_s
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+pp latitute = loc.fetch("lat")
+pp longitute = loc.fetch("lng")
+
+#currently_hash = parsed_response.fetch("currently")
+
+#current_temp = currently_hash.fetch("temperature")
+
+#puts "The current temperature is " + current_temp.to_s
